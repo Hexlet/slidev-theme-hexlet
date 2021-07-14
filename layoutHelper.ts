4c915e0e13d1286d44c06c7092244134f2e3e74b
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-14 17:14:25 +0400

    improve
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- layoutHelper.ts
+++ layoutHelper.ts
@@ -2,6 +2,7 @@ const preparedBackgroundUrls = {
   cover: 'https://raw.githubusercontent.com/Hexlet/assets/master/images/presentation-background-cover.png',
   center: 'https://raw.githubusercontent.com/Hexlet/assets/master/images/presentation-background-center.png',
   common: 'https://raw.githubusercontent.com/Hexlet/assets/master/images/presentation-background-common.png',
+  hexlet: 'https://raw.githubusercontent.com/Hexlet/assets/master/images/presentation-background-hexlet-next.png',
 };
 
 export function handleBackground(maybeUrl: string): CSSProperties {
