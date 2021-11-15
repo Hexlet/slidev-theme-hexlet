bddf2b76052e5b8a01b0c331cdac77d33831c4dd
Andrey Moshkov <fakesupp@gmail.com>
2021-11-15 11:46:06 +0300

    improve two cols layout

--- layouts/two-cols-right-full.vue
+++ layouts/two-cols-right-full.vue
@@ -16,10 +16,10 @@ const style = computed(() => handleBackground(props.background))
 
 <template>
   <div :style="style" class="slidev-layout two-columns w-full h-full grid grid-cols-2 p-0">
-    <div class="pt-10 pl-14 col-left flex flex-col" :class="props.class">
+    <div class="py-10 pl-14 pr-4 col-left flex flex-col" :class="props.class">
       <slot />
     </div>
-    <div class="col-right flex flex-col" :class="props.class">
+    <div class="col-right flex flex-col p-2" :class="props.class">
       <slot name="right" />
     </div>
   </div>
